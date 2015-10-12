94f54e18fe5eaffb7770f5ebbdd6c1e16a0f3150
Simon Kessler <simon.kessler@icloud.com>
2015-10-12 11:50:03 +0200

    Comment about immutability. Reordered constructor code

--- src/main/scala/org/sikessle/gameoflife/model/impl/GridImpl.scala
+++ src/main/scala/org/sikessle/gameoflife/model/impl/GridImpl.scala
@@ -5,15 +5,17 @@ import org.sikessle.gameoflife.model.Grid
 class GridImpl(cellsState: BitMatrix) extends Grid {
   private val cells = new BitMatrix(cellsState.rows, cellsState.columns)
 
-  override def rows: Int = cells.rows
-
-  override def columns: Int = cells.columns
-
+  // Copy values to ensure immutability
   for (i <- 0 until cells.rows) {
     for (j <- 0 until cells.columns) {
       cells(i)(j) = cellsState(i)(j)
     }
   }
+
+  override def rows: Int = cells.rows
+
+  override def columns: Int = cells.columns
+
 
   override def apply(row: Int): GridRow = new GridRowImpl(row)
 
